402589998acf61f2fee120fa9d535cfa11a46a05
BaeNayeong <55578809+BaeNayeong@users.noreply.github.com>
2021-02-08 16:06:52 +0900

    Create 10-1. 기본적인 서로소 집합 알고리즘.py
    이것이 취업을 위한 코딩 테스트다 10-1

---
+++ Graph/10-1. 기본적인 서로소 집합 알고리즘.py
@@ -0,0 +1,45 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    else:
+        return x
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선 입력
+v, e = map(int, input().split())
+
+parent = [0] * (v+1)
+
+for i in range(v+1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+print("각 원소가 속한 집합: ",end ='')
+
+for i in range(1, v+1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+#부모 테이블 출력
+print("부모 테이블 : ", end='')
+
+for i in range(1, v+1):
+    print(parent[i], end=' ')
